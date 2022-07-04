e93d863560390f309bf7ad7a427937b70d395cd8
Prarup Gurung <grgprarup@users.noreply.github.com>
2022-07-04 17:04:14 +0545

    set the router to the hash history mode (#5)
    * set the router to the hash history mode
    
    * updated the build code

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 
 const routes = [
   {
@@ -35,7 +35,7 @@ const routes = [
 
 const router = createRouter({
   // https://router.vuejs.org/guide/essentials/history-mode.html
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes // short for `routes: routes`
 })
 
