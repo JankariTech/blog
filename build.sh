60634554c6dd7d1fee0bdd5e937f65a71269cc52
Prarup Gurung <grgprarup@users.noreply.github.com>
2022-07-05 16:27:17 +0545

    added build script for publish (#13)
    * added build script for publish
    
    * Add cname if changes are there

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+BASE_DIR="home/runner/work/blog"
+DIST_BRANCH="dist"
+
+pnpm build
+
+mkdir -p "${BASE_DIR}""/temp"
+mv docs "${BASE_DIR}""/temp"
+
+git checkout $DIST_BRANCH
+
+rm -rf docs
+mv "${BASE_DIR}"/temp/docs .
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "blog.jankaritech.com" > docs/CNAME
+  git config user.name "GitHub Actions Bot"
+  git config user.email "<>"
+  git add .
+  git commit -m "Update the build code"
+  git push origin $DIST_BRANCH
+fi
+
+rm -rf "${BASE_DIR}""/temp"
