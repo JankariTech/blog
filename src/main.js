bc9709f073e6670dedf2025c4adece90224baf03
Kiran Parajuli <kiranparajuli589@gmail.com>
2022-08-26 12:08:59 +0545

    Add screen helper composable
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import router from "@/router"
 const pinia = createPinia()
 const app = createApp(App)
 
+app.config.globalProperties.window = window
+
 app.use(pinia)
 app.use(router)
 app.mount("#app")
